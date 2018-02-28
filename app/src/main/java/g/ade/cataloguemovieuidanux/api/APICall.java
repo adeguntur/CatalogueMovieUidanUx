7c19f65046461b19b168d7fd1bff1a78f381d31c
ade <adegunutur2001@gmail.com>
2018-02-28 22:59:12 +0700

    Initial commit

---
+++ app/src/main/java/g/ade/cataloguemovieuidanux/api/APICall.java
@@ -0,0 +1,31 @@
+
+package g.ade.cataloguemovieuidanux.api;
+
+import g.ade.cataloguemovieuidanux.model.NowPlayingModel;
+import g.ade.cataloguemovieuidanux.model.SearchModel;
+import g.ade.cataloguemovieuidanux.model.UpcomingModel;
+import g.ade.cataloguemovieuidanux.model.detail.DetailModel;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by omrobbie on 09/10/2017.
+ */
+
+public interface APICall {
+
+    @GET("movie/now_playing")
+    Call<NowPlayingModel> getNowPlayingMovie(@Query("language") String language);
+
+    @GET("movie/{movie_id}")
+    Call<DetailModel> getDetailMovie(@Path("movie_id") String movie_id, @Query("language") String language);
+
+    @GET("movie/upcoming")
+    Call<UpcomingModel> getUpcomingMovie(@Query("language") String language);
+
+    @GET("search/movie")
+    Call<SearchModel> getSearchMovie(@Query("query") String query, @Query("language") String language);
+}
